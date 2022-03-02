dae4a7b89ea2b36d70e8af77b7afc1f6ff17f9d9
Xushnora <xushnoranishonova@gmail.com>
2022-03-01 20:05:40 -0800

    initial comment

---
+++ integer1.js
@@ -0,0 +1,277 @@
+
+// ustoz bergan 2 ta masala
+
+// 1 - masala
+
+// let a = +prompt("a: ");
+// let b = +prompt("b: ");
+
+// let c = (a > b) ? a : b;
+
+// console.log(c);
+
+// 2-masala
+
+// let a = +prompt("a: ");
+// let b = +prompt("b: ");
+
+// let c = a == b ? "Teng" : a > b ? a : b;
+
+// console.log(c);
+
+// uy ishi   >>>>>>>>>> integer <<<<<<<<<<<
+
+// 1-masala
+
+// let a = +prompt("a: ");
+// let b = +prompt("b: ");
+
+// let s = Math.sqrt(Math.sqrt((a ** 3 - b ** 3) + (a ** 3 + b ** 3)));
+
+// console.log(s);
+
+// 2-masala
+ 
+// let a = +prompt("a: ");
+// let b = +prompt("b: ");
+
+// let c = (1/6 * Math.sqrt(a) +(1/3 * Math.sqrt(b)))**2;
+
+// console.log(c);
+
+// 3- masala
+
+// let a = +prompt("Son kiriting: ");
+// let num = +(a.toString().split('').reverse().join(''));
+
+// console.log(num);
+
+// 4-masala
+
+// let num = 568;
+// let numStr = num.toString();
+// let numArr = numStr.split("");
+// let num1 = numArr.shift();
+// let num2 = numArr.shift();
+// let num3 = numArr.shift();
+// let numResult = num2 + num1 + num3;
+
+// console.log(numResult);
+
+// 5-masala
+
+// let a = 6.3;
+// let b = 8.6;
+
+// let c = 9*(a**2*b) - 27*(a**2 * b**2) + 15*(b**2);
+
+// console.log(c.toFixed(2));
+
+// 6 - masala
+
+// let r = 3;
+// let n = 2;
+// let a = 4;
+// let b = 5;
+
+// let c = (1+ (r/100))**n / Math.sqrt(a*a + b*b);
+
+// console.log(c);
+
+// 7-masala
+
+// let x1 = -1.4;
+// let y1 = 7.9;
+// let x2 = 6.1;
+// let y2 = 9.9;
+
+// let c = Math.sqrt((x2 - x1)**2 + (y2 - y1)**2);
+
+// console.log(c.toFixed(2));
+
+// 8- masala
+
+// let x1 = -9.7;
+// let y1 = -9.9;
+// let x2 = 0.0;
+// let y2 = 2.9;
+// let x3 = -0.1;
+// let y3 = 5.2;
+
+// let a = 17.9;
+// let b = 2.3;
+// let c = 16;
+
+// let p = (a + b + c)/ 2;
+
+// let s = Math.sqrt(p*(p - a)* (p - b)* (p - c));
+
+// console.log(p, s);
+
+// 9 - masala
+
+// let a = -0.5;
+// let x = Math.sin(a); 
+
+// console.log(x);
+
+// 10 -masala
+
+// let a = 0.5;
+// let x = Math.cos(a); 
+
+// console.log(x);
+
+
+// >>>>>>>>>>>> boolen <<<<<<<<<<<<<<<
+
+// 1-masala
+
+
+// let a = +prompt("a: ");
+// let b = +prompt("b: ");
+
+// let c = a == b ? 'teng' : 'teng emas';
+
+// console.log(c);
+
+
+// 2-masala
+
+// let a = 7;
+// let b = +prompt("Iltimos, son kiriting");
+// let c = a == b ? "To'g'ri kiritdiz" : "Xato, boshqa son kiriting";
+
+// console.log(c);
+
+
+
+// 3-masala
+
+// let a = +prompt("Iltimos, musbat son kiriting: ")
+// let b = a > 0 ? 'true' : 'false';
+
+// console.log(b);
+
+// 4-masala
+
+// let a = +prompt("Iltimos, manfiy son kiriting: ")
+// let b = a < 0 ? 'true' : 'false';
+
+// console.log(b);
+
+
+// 5-masala
+
+// let a = +prompt("Iltimos, toq son kiriting: ")
+// let b = a % 2 == 1 ? 'true' : 'false';
+
+// console.log(b);
+
+// 6-masala
+
+// let a = +prompt("Iltimos, juft son kiriting: ")
+// let b = a % 2 == 0 ? 'true' : 'false';
+
+// console.log(b);
+
+
+
+// 7-masala
+
+// let a = +prompt("Iltimos, son kiriting: ");
+// let b = +prompt("Iltimos, son kiriting: ");
+// let c = +prompt("Iltimos, son kiriting: ");
+
+// let d = a < b < c ? 'true' : 'false';
+
+// console.log(d);
+
+
+// 8 - masala
+
+// let a = +prompt("Iltimos, son kiriting: ");
+// let b = +prompt("Iltimos, son kiriting: ");
+// let c = a > 0 && b > 0 ? 'true' : 'false';
+
+// console.log(c);
+
+// 9-masala
+
+// let a = +prompt("Iltimos, son kiriting: ");
+// let b = +prompt("Iltimos, son kiriting: ");
+// let c = a < 0 && b < 0 ? 'true' : 'false';
+
+// console.log(c);
+
+// 10-masala
+
+// let a = +prompt("Iltimos, son kiriting: ");
+// let b = +prompt("Iltimos, son kiriting: ");
+// let c = a % 2 == 0 && b % 2 == 0 ? 'true' : 'false';
+
+// console.log(c);
+
+
+// >>>>>>>>>>>> integer -2 <<<<<<<<<<<<<
+
+
+// 13-masala
+
+// let n = 5;
+// let m = ((3+n)/2)**2;
+
+// console.log(m);
+
+
+
+// 14 - masala
+
+// let n = 4;
+// let x = 2;
+// let c = Math.pow(n, x) + Math.pow(6, x);
+
+// console.log(c);
+
+
+
+// 15-masala
+
+// let a = +prompt("Iltimos, manfiy son kiriting: ");
+
+// console.log(Math.abs(a));
+
+
+// 16 - masala
+
+// let m = +prompt("Iltimos, son kiriting: ");
+
+// console.log(m.toFixed(2));
+
+
+
+// 17-masala
+
+// let x = +prompt("Iltimos, 1-son kiriting: ");
+// let y = +prompt("Iltimos, 2-son kiriting: ");
+// let c = Math.pow(x, 4) + 5*(Math.pow(x, 2)) + Math.pow(x, 3)*y;
+
+// console.log(c);
+
+
+// 18-masala
+
+// let x = 2;
+// let y = 3;
+// let c = 6 * Math.pow(x, 3) * Math.pow(y, 5) + 4*Math.pow(x, 4) * Math.pow(y, 3) + 24 * x * y;
+
+// console.log(c);
+
+// 19 - masala
+
+
+// 20- masala
+
+// let a = (1 + (2 / 3) - (3 + (1 / 4)));
+// console.log(Math.abs(a));
+    
