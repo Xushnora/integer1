470d1aa55d40d518e7378a44824d199dd7616cea
Xushnora <xushnoranishonova@gmail.com>
2022-03-02 19:47:02 -0800

    fors changed

---
+++ for1.js
@@ -0,0 +1,235 @@
+// let n = 1;
+
+// for (let i = 1; i <= l; i++) {
+//     console.log(i);
+// };
+
+// let n = 10;
+// let a = 2;
+// let s = 0;
+
+// for (let i = 0; i <= n; i++) {
+//     s += a**i;
+// }
+// console.log(s);
+ 
+
+// 1-masala
+
+// let n = 9;
+// let k = 6;
+
+// for (let i = 0; i < k; i++){
+//     console.log(n);
+// };
+
+// 2-masala
+
+// let a = 2
+// let b = 5
+// let count = 0
+// for(let i=a; i<=b; i++){
+//     console.log(i);
+//     count++
+// }
+// console.log(count);
+
+
+// 3-masala
+
+
+// let a = 10;
+// let b = 20;
+// let count = 0;
+
+// for(let i = b-1; i > a; i--){
+//     console.log(i);
+//     count++;
+// }
+// console.log(count, "marta");
+
+
+// 4-masala
+
+// let narx = 10300;
+
+// for (let i = 1; i <= 10; i += 2) {
+//     console.log(`${i} kg konfet narxi ${i * narx} so'm`);
+// };
+
+// 5-masala
+
+// let narx = 10300;
+
+// for (let i = 0.1; i <= 1; i += 0.1) {
+//     console.log(`${i.toFixed(1)} kg konfet narxi ${(i * narx).toFixed(0)} so'm`);
+// };
+
+// 6 -masala
+
+// let narx = 10300;
+
+// for (let i = 1.2; i <= 2; i += 0.2) {
+//     console.log(`${i.toFixed(1)} kg konfet narxi ${(i * narx).toFixed(0)} so'm`);
+// };
+
+// 7-masala
+
+// let a = 1;
+// let b = 10;
+// let count = 0;
+// for (let i = a; i < b; i++) {
+//     console.log(i);
+//     count += i;
+// }
+// console.log("Yig'indi: ",count);
+
+
+// 8-masala
+
+// let a = 1;
+// let b = 10;
+// let count = 1;
+// for (let i = a; i < b; i++) {
+//     console.log(i);
+//     count *= i;
+// }
+// console.log("Ko'paytma: ",count);
+
+
+//  9-masala
+
+// let a = 1;
+// let b = 10;
+// let count = 0;
+// let kv = 1;
+// for (let i = a; i < b; i++) {
+//     // console.log(i);
+//     kv = i * i;
+//     console.log(i, "kvadrati: ", kv)
+//     count += kv;
+// }
+// console.log("Yig'indisi: ",count);
+
+// 10-masala
+
+// let n = 50;
+// let S = 0;
+// for (let i = 1; i <= n; i++) {
+//     S += 1 / i;
+// };
+// console.log(S.toFixed(2));
+
+// 11-masala
+
+// let n = 5;
+// let S = (n**2);
+// for (let i = 1; i < (n**2); i++) {
+//     S += (n+i)**2; 
+// };
+// console.log(S);
+
+// 12- masala 
+
+// let n = 2;
+// let summa = 1;
+
+// for (let i = 1.1; i <= n; i += 0.1) {
+//     console.log(i.toFixed(1));
+//     summa *= i;
+// }
+// console.log(summa.toFixed(2));
+
+// 13-masala
+
+// let a = 2;
+// let sum = 0;
+// let first = a - 1;
+// for (let i = 1; i <= 10; i++) {
+//     first+=0.1;
+//     (i % 2 == 0) ? sum += first : sum -= first;
+// }
+// console.log(sum.toFixed(1));
+
+
+// 14-masala
+
+
+// let n = 5;
+
+// for (let i = 1; i < (2*n); i += 2){
+//     console.log(i*i);
+// }
+
+// 15-masala
+
+// 16-masala
+
+// let a = 2;
+// let n = 5;
+// let count = 1;
+// for (let i = 1; i <= n; i++) {
+    
+//     count = (a**i);
+//     console.log(count);
+
+// };
+
+// 17-masala
+
+// let a = 2;
+// let n = 5;
+// let count = 1;
+// let yig = 0;
+// for (let i = 1; i <= n; i++) {
+//     count = (a**i);
+//     console.log(count);
+//     yig += (a**i);
+
+// };
+// console.log("Yig'indi: ", yig);
+
+// 18-masala
+
+
+// 19-masala
+
+// let n = 7;
+// let s = 1;
+
+// for (let i = 1; i <= n; i++) {
+//     s *= i;
+// }
+
+// console.log(s);
+
+
+// 20-masala
+
+// let n = 5;
+// let s = 0;
+// for (let i = 1; i <= n; i++) {
+//     let k = 1;
+//     for (let j = 1; j <= i; j++) {
+//         k *= j;
+//     }
+//     console.log(k);
+//     s += k;
+// }
+// console.log("Yig'indi: ", s);
+
+
+// 21-masala
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ integer2.js
@@ -0,0 +1,87 @@
+// 3-masala
+
+// let a = 70;
+// let b = 70;
+
+// if (a < b) {
+//     console.log(b);
+// } else if (b < a) {
+//     console.log(a);
+// } else {
+//     console.log(0);
+// }
+
+// 5-masala
+
+// let x = -5;
+
+// if (x > 0) {
+//     console.log(2 * Math.sin(x));
+// } else if (x <= 0) {
+//     console.log(x-6);
+// };
+
+// 6-masala
+
+// let x = 1;
+
+// if (x < -2 || x > 2) {
+//     console.log(2 * x);
+// } else {
+//     console.log(-3 * x);
+// };
+
+// 8 - masala
+
+// let a = 7;
+// let b = 4;
+// let c = 2;
+
+// if ((a + b > c) && (b + c > a)) {
+//     console.log("Yes");
+// } else {
+//     console.log("No");
+// };
+
+//  9 - masala
+
+// let temp = 10;
+
+// if (temp < 0) {
+//     console.log("Freezing");
+// } else if (1 <= temp <= 10) {
+//     console.log("Very Cold");
+// } else if (11 <= temp <= 20) {
+//     console.log("Cold");
+// } else if (21 <= temp <= 30) {
+//     console.log("Normal");
+// } else if (31 <= temp <= 40) {
+//     console.log("Hot");
+// } else {
+//     console.log("Very Hot");
+// };
+
+// 10-masala
+
+// let a = 0;
+
+// if ((a > 0) && (a % 2 == 1)) {
+//     console.log("musbat, toq son");
+// } else if ((a > 0) && (a % 2 == 0)) {
+//     console.log("musbat, juft son");
+// } else if ((a < 0) && (a % 2 == 1)) {
+//     console.log("manfiy, toq son");
+// } else if ((a < 0) && (a % 2 == 0)) {
+//     console.log("manfiy, juft son");
+// } else {
+//     console.log("Son 0 ga teng");
+// };
+
+
+//  11- masala
+
+
+
+
+
+
